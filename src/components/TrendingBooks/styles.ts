34e01ebd9bca35dd3b15aee78cf53ca497372bbf
c1334690 <joao.ribeiro@capgemini.com>
2023-12-13 23:54:48 -0300

    feat(bw04-trending-books): implementa lista de livros mais comentados

---
+++ src/components/TrendingBooks/styles.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 324px;
+  margin-left: 64px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+`;
+
+export const HeaderTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`;
+
+export const Title = styled.h1`
+  color: ${(props) => props.theme.white};
+  font-family: 'Nunito Sans';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%; /* 22.4px */
+`;
+
+export const SeeButton = styled.button`
+  background-color: transparent !important;
+  border: none;
+  color: ${(props) => props.theme['purple-100']};
+  font-family: 'Nunito Sans';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 160%; /* 22.4px */
+`;
+
+export const CardBook = styled.div`
+  background: ${(props) => props.theme['gray-700']};
+  display: flex;
+  padding: 16px 20px;
+  align-items: center;
+  gap: 20px;
+  align-self: stretch;
+  border-radius: 8px;
+`;
+
+export const CardBookTitle = styled.p`
+color: ${(props) => props.theme.white};
+font-family: 'Nunito Sans';
+font-size: 16px;
+font-style: normal;
+font-weight: 700;
+line-height: 140%; /* 22.4px */
+`;
+
+export const BookAuthor = styled.p`
+  color: ${(props) => props.theme['gray-400']};
+  font-family: 'Nunito Sans';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%; /* 22.4px */
+`;
+
+export const Rating = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 34px;
+  gap: 4px;
+`;
